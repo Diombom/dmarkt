52df646f1c5d45f56fd6a9bf48e99b7ae1da1f9f
Diom Brandon <brandondiom@gmail.com>
2024-10-15 12:32:54 -0700

    commit

--- src/app/compontents/Categories.tsx
+++ src/app/compontents/Categories.tsx
@@ -1,6 +1,27 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+
 const Categories = () => {
+
+    const categories = [ 1, 2, 3, 4, 5, 6, 7, 8]
+
     return (
-        <div className="">Categories</div>
+        <div className="px-4 overflow-x-scroll">
+            <div className="flex gap-4 md:gap-8">
+                {
+                    categories.map(item => (
+                        <Link href="/list?cat=test" className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6">
+                            <div className="relative bg-slate-100 w-full h-72">
+                                <Image src="/products/sunglasses.jpg" alt="" fill sizes="20vw" className="object-cover" />
+                            </div>
+                            <h2 className="mt-4 font-light text-cl tracking-wide">Category Name</h2>
+                        </Link>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const HomePage = () => {
         <h2 className="text-2xl font-semibold">Featured Products</h2>
         <ProductList />
       </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <div className="mt-24">
         <h2 className="text-2xl font-semibold">Categories</h2>
         <Categories />
       </div>
